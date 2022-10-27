19a3c227af65af15f61d9f1d261e07891af1890e
AK-Polash <polashk199@gmail.com>
2022-10-27 06:17:58 +0600

    layout figured out

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+let userName = document.querySelector("#user__name");
+let userPost = document.querySelector("#user__post");
+let userSubmitButton = document.querySelector(".submit__button");
+
+let name = document.querySelector(".name");
+let post = document.querySelector(".post");
+
+let deleteButton = document.querySelector("#delete__button");
+let editButton = document.querySelector("#edit__button");
+let shareButton = document.querySelector("#share__button");
+
+
