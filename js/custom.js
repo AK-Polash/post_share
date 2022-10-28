0bf1b64975e71706ec3a88da2da0c889bb416d64
AK-Polash <polashk199@gmail.com>
2022-10-28 14:20:22 +0600

    edit problem added here
    Edit button does not work properly in multiple array element. I think the problem might be on the random changing value of the index number while it starts from 0 and keep incrementing as we keep pressing the edit button the problem occurs....

--- js/custom.js
+++ js/custom.js
@@ -2,28 +2,30 @@
 let userName = document.querySelector("#user__name");
 let userPost = document.querySelector("#user__post");
 let userSubmitButton = document.querySelector(".submit__button");
+let saveButton = document.querySelector(".save__button");
+saveButton.style.display = "none";
 
-// inner html:
+// inside html:
 let poster = document.querySelector(".name");
 let postDetails = document.querySelector(".post");
+
 // post buttons:
-let editButton = document.querySelector(".edit__button");
 let shareButton = document.querySelector(".share__button");
 
 let mainArea = document.querySelector(".main__area");
-let mainArray = [];
+let mainArr = [];
 
 userSubmitButton.addEventListener("click", () => {
-  mainArray.push({ name: `${userName.value}`, post: `${userPost.value}` });
+  mainArr.push({ name: userName.value, post: userPost.value });
   userName.value = "";
   userPost.value = "";
   postme();
-  console.log(mainArray);
+  console.log(mainArr);
 });
 
 function postme() {
   mainArea.innerHTML = "";
-  mainArray.map((item) => {
+  mainArr.map((item) => {
     mainArea.innerHTML += ` <div class="box"> <div class="name__box"> <h1 class="name"> ${item.name} </h1> </div> <div class="post__box"> <p class="post">${item.post}</p> </div> <div class="button__box"> <button class="button__item delete__button" type="button">Delete</button> <button class="button__item edit__button" type="button">Edit</button> <button class="button__item share__button" type="button">Share</button> </div> </div> `;
   });
 
@@ -32,9 +34,37 @@ function postme() {
   let deleteArr = Array.from(deleteButton);
   deleteArr.map((buttonItem, index) => {
     buttonItem.addEventListener("click", () => {
-      mainArray.splice(index, 1);
-      postme()
-      console.log(mainArray);
+      mainArr.splice(index, 1);
+      postme();
+      console.log(mainArr);
     });
+  });
+
+  // Edit Button:
+  let editButton = document.querySelectorAll(".edit__button");
+  let editArr = Array.from(editButton);
+  editArr.map((buttonItem, index) => {
+    buttonItem.addEventListener("click", () => {
+      userName.value = mainArr[index].name;
+      userPost.value = mainArr[index].post;
+      userSubmitButton.style.display = "none";
+      saveButton.style.display = "block";
+      postme();
+      console.log(mainArr);
+      // console.log(index);
+
+      // Save Button:
+      saveButton.addEventListener("click", () => {
+        saveButton.style.display = "none";
+        userSubmitButton.style.display = "block";
+        mainArr.splice(index, 1, {
+          name: userName.value,
+          post: userPost.value,
+        });
+        postme();
+        console.log(index);
+      });
+    });
+    // eikhane save button er code gula rakhle kaj kore na keno..?
   });
 }
